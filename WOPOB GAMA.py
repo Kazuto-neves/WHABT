3ff34c2a12d27e827d5ae60c9a54145e5be64704
kazutolure <chrsmneves@gmail.com>
2020-09-14 14:12:33 -0300

    Update WOPOB GAMA.py

--- WOPOB GAMA.py
+++ WOPOB GAMA.py
@@ -6,22 +6,22 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(ChromeDriverManager().install())
 import PySimpleGUI as sg
 
-    def buscar_contato(contato):# buscando nome
-        compo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-        time.sleep(3)
-        compo_pesquisa.click()
-        compo_pesquisa.send_keys(contato)
-        compo_pesquisa.send_keys(Keys.ENTER)
-    def enviar_mensagem(mensagem):# buscando mensagem
-        campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-        campo_mensagem[1].click()
-        time.sleep(3)
-        campo_mensagem[1].send_keys(mensagem)
-        campo_mensagem[1].send_keys(Keys.ENTER)
-    for contato in contatos:#fazer o envio
-        buscar_contato(contato)
-        enviar_mensagem(mensagem)
-  class wppbot:
+def buscar_contato(contato):# buscando nome
+    compo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    time.sleep(3)
+    compo_pesquisa.click()
+    compo_pesquisa.send_keys(contato)
+    compo_pesquisa.send_keys(Keys.ENTER)
+def enviar_mensagem(mensagem):# buscando mensagem
+    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    campo_mensagem[1].click()
+    time.sleep(3)
+    campo_mensagem[1].send_keys(mensagem)
+    campo_mensagem[1].send_keys(Keys.ENTER)
+for contato in contatos:#fazer o envio
+    buscar_contato(contato)
+    enviar_mensagem(mensagem)
+class wppbot:
     #interface
     def __init__(self):
         layout = [
@@ -31,15 +31,12 @@ import PySimpleGUI as sg
         ]
         janela = sg.Window("WOPOB").layout(layout)
         self.button, self.values = janela.Read()
-
     def Iniciar(self):
-        nome = self.values['nome']# digite o nome
-        texto  = self.values['mensagem']# digite a mensagem
+        contatos = self.values['nome']# digite o nome
+        mensagem = self.values['mensagem']# digite a mensagem
         driver.get('https://web.whatsapp.com/')#site
         time.sleep(30)# time
         #passando dados
-        contatos = nome 
-        mensagem = texto
         #execultando interface
 tela = wppbot()
 tela.Iniciar()
