811dd8ae3e75038a2989b6ce8aead4c45b5651b4
kazutolure <chrsmneves@gmail.com>
2020-12-22 16:04:42 -0300

    up

--- tela.py
+++ tela.py
@@ -8,41 +8,23 @@ layout =  [
 window = sg.Window('Escolha', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 event, values = window.read()
-if values['1']==True: # if user closes window or clicks cancel
-    sg.theme('lightGreen3')# Add a touch of color
+# if user closes window or clicks cancel
+sg.theme('lightGreen3') if values['1']==True else sg.theme('DarkGreen4')
     # All the stuff inside your window.
-    layouts = [[sg.Image(r"E:\git\pytom\WOPOB\resources\branco.png")],
-               [sg.Text('Nome:        '), sg.InputText(key='nome',size=(50,1))],
-               [sg.Text('Mensagem:'),sg.Multiline(size=(50, 3),key='men')],
-               [sg.Button('Ok'),
-               sg.Button('Cancel')]]
+    
+layouts = [[sg.Image(r"E:\git\pytom\WOPOB\resources\branco.png")],
+           [sg.Text('Nome:        '), sg.InputText(key='nome',size=(50,1))],
+           [sg.Text('Mensagem:'),sg.Multiline(size=(50, 3),key='men')],
+           [sg.Button('Ok'),
+           sg.Button('Cancel')]]
 
 # Create the Window
-    window2 = sg.Window('WHABT', layouts)
+window2 = sg.Window('WHABT', layouts)
 # Event Loop to process "events" and get the "values" of the inputs
-    while True:
-        event, values = window2.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-            break
-        print('Nome: ', values['nome'])
-        print('Mensagem: ',values['men'])
-    window2.close()
-else:
-    sg.theme('DarkGreen4')   # Add a touch of color
-# All the stuff inside your window.
-    layouts = [[sg.Image(r"E:\git\pytom\WOPOB\resources\preto.png")],
-               [sg.Text('Nome:        '), sg.InputText(key='nome',size=(50,1))],
-               [sg.Text('Mensagem:'),sg.Multiline(size=(50, 3),key='men')],
-               [sg.Button('Enviar'),
-               sg.Button('Sair')] ]
-
-# Create the Window
-    window2 = sg.Window('WHABT', layouts)
-# Event Loop to process "events" and get the "values" of the inputs
-    while True:
-        event, values = window2.read()
-        if event == sg.WIN_CLOSED or event == 'Sair': # if user closes window or clicks cancel
-            break
-        print('Nome: ', values['nome'])
-        print('Mensagem: ',values['men'])
-    window2.close()
+while True:
+    event, values = window2.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('Nome: ', values['nome'])
+    print('Mensagem: ',values['men'])
+window2.close()
