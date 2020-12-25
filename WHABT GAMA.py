43c08d8ab1a598609ee56b13ecce1bff9c177351
kazutolure <chrsmneves@gmail.com>
2020-12-24 21:49:02 -0300

    Update WHABT GAMA.py

--- WHABT GAMA.py
+++ WHABT GAMA.py
@@ -3,6 +3,7 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import PySimpleGUI as sg
+#import datetime
 def Escolha():
         b=0
         layout =  [
@@ -15,16 +16,18 @@ def Escolha():
                 # if user closes window or clicks cancel
         sg.theme('lightGreen3') if values['1']==True else sg.theme('DarkGreen4')
                 # All the stuff inside your window.
+        #Today = datetime.date.today()
         #People_layouts =  [[sg.T('This is inside tab 1')]]
         #Nature_layouts = [[sg.T('This is inside tab 2')]]
         #Objects_layouts = [[sg.T('This is inside tab 3')]]
         #Places_layouts = [[sg.T('This is inside tab 4')]]
         #Symbols_layouts = [[sg.T('This is inside tab 5')]]
         layouts = [[sg.Image(r"E:\git\pytom\WOPOB\resources\branco.png") if values['1']==True else sg.Image(r"E:\git\pytom\WOPOB\resources\preto.png")],
+                #[sg.Text("Hoje:"),sg.Print('{}'.format('Today.now'))],
                 [sg.Text('Nome:        '), sg.InputText(key='nome',size=(50,1))],
                 [sg.Text('Mensagem:'),sg.Multiline(size=(50, 3),key='men')],
                 #[sg.Checkbox('emoji',key='3')],
-                # sg.TabGroup([[sg.Tab('Tab 1',People_layouts), sg.Tab('Tab 2',Nature_layouts),sg.Tab('Tab 3',Objects_layouts), sg.Tab('Tab 4',Places_layouts),sg.Tab('Tab 5',Symbols_layouts)]])if values['3']==True else ''],
+                #[sg.TabGroup([[sg.Tab('Tab 1',People_layouts), sg.Tab('Tab 2',Nature_layouts),sg.Tab('Tab 3',Objects_layouts), sg.Tab('Tab 4',Places_layouts),sg.Tab('Tab 5',Symbols_layouts)]])],
                 [sg.Button('Ok'),
                 sg.Button('Cancel')]]
 
@@ -40,32 +43,30 @@ def Escolha():
         while True:
                 event, values = window2.read()
                 if event == sg.WIN_CLOSED or event == 'Cancel':break
-                if event == 'OK':
-                        c=values['nome']
-                        contatos = [c]
-                        s=values['men']
-                        mensagem  = [s]
-                        window.close()
-                        if b==0:
-                                driver = webdriver.Chrome(ChromeDriverManager().install())
-                                driver.get('https://web.whatsapp.com/')#site
-                                b=1
-                        time.sleep(30)# time
-                        def buscar_contato(contato):# buscando nome
-                                compo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-                                time.sleep(2)
-                                compo_pesquisa.click()
-                                compo_pesquisa.send_keys(contato)
-                                compo_pesquisa.send_keys(Keys.ENTER)
-                        def enviar_mensagem(mensagem):# buscando mensagem
-                                campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-                                campo_mensagem[1].click()
-                                time.sleep(2)
-                                campo_mensagem[1].send_keys(mensagem)
-                                campo_mensagem[1].send_keys(Keys.ENTER)
-                        for contato in contatos:#fazer o envio
-                                buscar_contato(contato)
-                                enviar_mensagem(mensagem)
-                                sg.SystemTray.notify('Mensagem enviada')
-                window2.close()
+                c=values['nome']
+                contatos = [c]
+                s=values['men']
+                mensagem  = [s]
+                window.close()
+                if b==0:
+                        driver = webdriver.Chrome(ChromeDriverManager().install())
+                        driver.get('https://web.whatsapp.com/')#site
+                        b=1
+                time.sleep(30)# time
+                def buscar_contato(contato):# buscando nome
+                        compo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+                        time.sleep(2)
+                        compo_pesquisa.click()
+                        compo_pesquisa.send_keys(contato)
+                        compo_pesquisa.send_keys(Keys.ENTER)
+                def enviar_mensagem(mensagem):# buscando mensagem
+                        campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+                        campo_mensagem[1].click()
+                        time.sleep(2)
+                        campo_mensagem[1].send_keys(mensagem)
+                        campo_mensagem[1].send_keys(Keys.ENTER)
+                for contato in contatos:#fazer o envio
+                        buscar_contato(contato)
+                        enviar_mensagem(mensagem)
+        window2.close()
 Escolha()
